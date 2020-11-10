6cbb05cf10544f8e0faf518cdba0dc0664a85882
431865884@qq.com <hipos123456>
2020-11-10 14:12:25 +0800

    新增test文件

---
+++ src/main/java/com/yaoxj/test/BaseTest.java
@@ -0,0 +1,15 @@
+package com.yaoxj.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @description:
+ * @author: yaoxj
+ * @create: 2020-11-10 11:21
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BaseTest {
+}
