010ee787f6b91a0d20d7f5c728547f0554b050f3
431865884@qq.com <hipos123456>
2020-11-16 20:53:02 +0800

    修改扫描文件

--- src/main/java/com/yaoxj/App.java
+++ src/main/java/com/yaoxj/App.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@MapperScan("com.joy.*")
+@MapperScan("com.yaoxj.*")
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
