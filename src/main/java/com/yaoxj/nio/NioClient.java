353249fa264528a3c2cecce1f61b5badd807025e
yaoxiangjian <yaoxiangjian@bosssoft.com.cn>
2021-01-12 09:02:54 +0800

    nio server and client

---
+++ src/main/java/com/yaoxj/nio/NioClient.java
@@ -0,0 +1,28 @@
+package com.yaoxj.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+
+public class NioClient {
+    public static void main(String[] args) throws IOException {
+
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.configureBlocking(false);
+        Selector selector = Selector.open();
+        InetSocketAddress inetSocketAddress=new InetSocketAddress("127.0.0.1",6666);
+        if (!socketChannel.connect(inetSocketAddress)) {
+            while (!socketChannel.finishConnect()){
+                System.out.println("没有连接到服务端，可能连接需要事件，可以做其他事情");
+            }
+        }
+        String str="hello,nio,my first demo";
+        ByteBuffer byteBuffer=ByteBuffer.wrap(str.getBytes());
+        socketChannel.write(byteBuffer);
+        //代码停在这边
+        System.in.read();
+    }
+}
---
+++ src/main/java/com/yaoxj/nio/NioServer.java
@@ -0,0 +1,55 @@
+package com.yaoxj.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
+
+public class NioServer {
+    public static void main(String[] args) throws IOException {
+
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+
+        Selector selector = Selector.open();
+
+        serverSocketChannel.socket().bind(new InetSocketAddress(6666));
+        serverSocketChannel.configureBlocking(false);
+        //注册到selector上，第二个参数是注册的事件
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        while(true){
+            //选择器的select是阻塞事件，等待1秒，是否有事件发，如果没有事件发生，可以做自己的事情
+            if (selector.select(1000)==0) {
+                System.out.println("服务器等待了1秒，无事件发生");
+                continue;
+            }
+
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = selectionKeys.iterator();
+            while(iterator.hasNext()){
+                SelectionKey selectionKey = iterator.next();
+                //根据key ，查看通道发生的事情
+                if (selectionKey.isAcceptable()) {
+                    SocketChannel socketChannel= serverSocketChannel.accept();
+                    socketChannel.configureBlocking(false);
+                    socketChannel.register(selector,SelectionKey.OP_READ, ByteBuffer.allocate(1024));
+                    System.out.println("服务端接收到了客户端的连接:"+socketChannel.hashCode());
+                }
+
+                if(selectionKey.isReadable()){
+                    SocketChannel socketChannel= (SocketChannel) selectionKey.channel();
+                    //获取到该channel关联的buffer
+                    ByteBuffer byteBuffer = (ByteBuffer) selectionKey.attachment();
+                    socketChannel.read(byteBuffer);
+                    System.out.println("服务端获取到的数据是："+new String(byteBuffer.array()));
+                }
+                iterator.remove();
+            }
+
+
+        }
+
+    }
+
+}
