4d56c486da45c81b635305f88d4f09635c32ba90
yaoxiangjian <yaoxiangjian@bosssoft.com.cn>
2021-01-17 23:03:24 +0800

    group chat

--- src/main/java/com/yaoxj/nio/groupchat/ChatClient.java
+++ src/main/java/com/yaoxj/nio/groupchat/ChatClient.java
@@ -8,6 +8,7 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
+import java.util.Scanner;
 import java.util.Set;
 
 /**
@@ -20,12 +21,18 @@ public class ChatClient {
     private static final int PORT=6666;
     private Selector selector=null;
     private SocketChannel socketChannel=null;
+    String username=null;
 
     public ChatClient() throws IOException {
-        selector=Selector.open();
-        socketChannel=SocketChannel.open(new InetSocketAddress(HOST,PORT));
+        selector = Selector.open();
+        //连接服务器
+        socketChannel =  SocketChannel.open(new InetSocketAddress(HOST,PORT));
+        //设置非阻塞
         socketChannel.configureBlocking(false);
-        socketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        //将channel注册到selector上
+        socketChannel.register(selector, SelectionKey.OP_READ);
+        username = socketChannel.getLocalAddress().toString().substring(1);
+        System.out.println(username + " is ok....");
     }
 
     public void readInfo() throws IOException {
@@ -38,14 +45,44 @@ public class ChatClient {
                 ByteBuffer byteBuffer=ByteBuffer.allocate(1024);
                 channel.read(byteBuffer);
                 System.out.println("从服务端读取到的数据是："+new String(byteBuffer.array()));
+                iterator.remove();
             }
+
         }
     }
 
-    public void writeInfo2Server() throws IOException {
-        String str=socketChannel.getRemoteAddress()+":hello,nio";
+    public void writeInfo2Server(String msg) throws IOException {
+        String str=username+"说:"+msg;
         ByteBuffer byteBuffer=ByteBuffer.wrap(str.getBytes());
         socketChannel.write(byteBuffer);
+    }
+
+    public static void main(String[] args) throws IOException {
+        ChatClient client=new ChatClient();
+        new Thread() {
+            @Override
+            public void run() {
+                while(true){
+                    try {
+                        client.readInfo();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        Thread.currentThread().sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        }.start();
+
+        Scanner scanner=new Scanner(System.in);
+        while (scanner.hasNext()){
+            String s = scanner.nextLine();
+            client.writeInfo2Server(s);
+        }
     }
 
 
--- src/main/java/com/yaoxj/nio/groupchat/ChatServer.java
+++ src/main/java/com/yaoxj/nio/groupchat/ChatServer.java
@@ -24,14 +24,22 @@ public class ChatServer {
 
     public void AcceptClientConnect() throws IOException {
         while(true){
-            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
-            while(iterator.hasNext()){
-                SelectionKey selectionKey = iterator.next();
-                if(selectionKey.isAcceptable()){
-                    SocketChannel socketChannel = serverSocketChannel.accept();
-                    socketChannel.configureBlocking(false);
-                    socketChannel.register(selector,SelectionKey.OP_READ);
-                    System.out.println(socketChannel.getRemoteAddress()+"上线了");
+            int select = selector.select();
+            if(select>0){
+                Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                while(iterator.hasNext()){
+                    SelectionKey selectionKey = iterator.next();
+                    if(selectionKey.isAcceptable()){
+                        SocketChannel socketChannel = serverSocketChannel.accept();
+                        socketChannel.configureBlocking(false);
+                        socketChannel.register(selector,SelectionKey.OP_READ);
+                        System.out.println(socketChannel.getRemoteAddress()+"上线了");
+                    }
+
+                    if (selectionKey.isReadable()){
+                        readMsgFromClient();
+                    }
+                    iterator.remove();
                 }
             }
         }
@@ -49,16 +57,36 @@ public class ChatServer {
         }
     }
 
-    public void readMsgFromClient() throws IOException {
+    public void readMsgFromClient()  {
         Set<SelectionKey> keys = selector.keys();
         for(SelectionKey key:keys){
-            SocketChannel channel = (SocketChannel) key.channel();//通过selectionKey反向获取channel
-            ByteBuffer byteBuffer=ByteBuffer.allocate(1024);
-            int read = channel.read(byteBuffer);
-            if(read>0){
-                System.out.println("从客户端获取到的消息是："+new String(byteBuffer.array()));
+            Channel channel = key.channel();//通过selectionKey反向获取channel
+            if(channel  instanceof  SocketChannel ){
+                SocketChannel MymyChannel=(SocketChannel)channel;
+                ByteBuffer byteBuffer=ByteBuffer.allocate(1024);
+                try {
+                    int read = MymyChannel.read(byteBuffer);
+                    if(read>0){
+                        String msg = new String(byteBuffer.array());
+                        System.out.println("从客户端获取到的消息是："+msg);
+                        sendMsg2OtherChannel(msg,MymyChannel);
+                    }
+                } catch (IOException e) {
+                    try {
+                        System.out.println(MymyChannel.getRemoteAddress()+"下线了");
+                        MymyChannel.close();
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                }
+
             }
         }
 
+    }
+
+    public static void main(String[] args) throws IOException {
+        ChatServer chatServer=new ChatServer();
+        chatServer.AcceptClientConnect();
     }
 }
